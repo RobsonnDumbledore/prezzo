6acdb224bf3b75b875f9cb3db3f22b5aaa27c30a
Robsonndumbledore <robsonndumbledore@gmail.com>
2021-01-04 13:09:14 -0300

    projeto inicial

---
+++ src/main/java/br/com/noblesse/prezzo/PrezzoApplication.java
@@ -0,0 +1,13 @@
+package br.com.noblesse.prezzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrezzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrezzoApplication.class, args);
+	}
+
+}
