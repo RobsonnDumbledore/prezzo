69b1649e8fe3c91c94a9dff8983227645887cfaa
Robsonndumbledore <robsonndumbledore@gmail.com>
2021-01-04 18:43:45 -0300

    feat: criar entidade cotacao

---
+++ src/main/java/br/com/noblesse/prezzo/entities/Cotacao.java
@@ -0,0 +1,42 @@
+package br.com.noblesse.prezzo.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Robson
+ */
+@Getter
+@Setter
+@Entity
+@Table(name = "cotacao")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cotacao implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "empresa_id")
+    private Empresa empresa;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
+
+    private Float preco;
+
+}
