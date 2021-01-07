c95f76d55a3f5ecc0a1ce64bce29556e15efa3bd
Robsonndumbledore <robsonndumbledore@gmail.com>
2021-01-07 16:08:59 -0300

    feat: implementar autenticacao basica

---
+++ src/main/java/br/com/noblesse/prezzo/security/JwtService.java
@@ -0,0 +1,69 @@
+package br.com.noblesse.prezzo.security;
+
+import br.com.noblesse.prezzo.entities.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Robson
+ */
+@Service
+@Getter
+@Setter
+public class JwtService {
+
+    @Value("${security.jwt.expiration}")
+    private String expiration;
+
+    @Value("${security.jwt.subscription-key}")
+    private String subscriptionKey;
+
+    public String generateToken(Usuario usuario) {
+        long expString = Long.valueOf(this.expiration);
+        LocalDateTime expirationDate = LocalDateTime.now().plusMinutes(expString);
+        Instant instant = expirationDate.atZone(ZoneId.systemDefault()).toInstant();
+        Date data = Date.from(instant);
+        return Jwts
+                .builder()
+                .setSubject(usuario.getEmail())
+                .setExpiration(data)
+                .signWith(SignatureAlgorithm.HS512, subscriptionKey)
+                .compact();
+    }
+
+    private Claims getClaims(String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(subscriptionKey)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public Boolean validToken(String token) {
+        try {
+            Claims claims = getClaims(token);
+            Date expirationDate = claims.getExpiration();
+            LocalDateTime data = expirationDate.toInstant()
+                    .atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return !LocalDateTime.now().isAfter(data);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getEmailByUser(String token) throws ExpiredJwtException {
+        return (String) getClaims(token).getSubject();
+    }
+
+}
--- src/main/java/br/com/noblesse/prezzo/services/UsuarioService.java
+++ src/main/java/br/com/noblesse/prezzo/services/UsuarioService.java
@@ -5,6 +5,11 @@ import br.com.noblesse.prezzo.exceptions.DuplicateKeyException;
 import br.com.noblesse.prezzo.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import br.com.noblesse.prezzo.repositories.UsuarioRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.stereotype.Service;
 
@@ -13,38 +18,54 @@ import org.springframework.stereotype.Service;
  * @author Robson
  */
 @Service
-public class UsuarioService {
-
+public class UsuarioService implements UserDetailsService {
+    
     @Autowired
     private UsuarioRepository repository;
-
+    
+    @Autowired
+    private PasswordEncoder encoder;
+    
     public Usuario save(Usuario usuario) {
         existsByEmail(usuario.getEmail());
+        usuario.setSenha(encoder.encode(usuario.getSenha()));
         return repository.save(usuario);
     }
-
+    
     public Usuario update(Usuario usuario) {
         return repository.save(usuario);
     }
-
+    
     public Usuario findById(Long id) {
         return repository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Usuario não encontrado"));
     }
-
+    
     public Usuario findByEmail(String email) {
         return repository.findByEmail(email)
                 .orElseThrow(() -> new EntityNotFoundException("Usuario não encontrado"));
     }
-
+    
     public void existsByEmail(String email) {
         if (repository.existsByEmail(email)) {
-            throw new DuplicateKeyException("email já cadastrado");
+            throw new DuplicateKeyException("Email já cadastrado");
         }
     }
-
+    
     public void delete(Long id) {
         repository.deleteById(id);
     }
-
+    
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = repository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado"));
+        return User
+                .builder()
+                .username(usuario.getEmail())
+                .roles()
+                .password(usuario.getSenha())
+                .build();
+    }
+    
 }
